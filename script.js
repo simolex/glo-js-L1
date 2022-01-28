f3c826bd82bd9e1599eb3adbd837cb53dcf2f839
simolex <simolex@yandex.ru>
2022-01-28 09:41:30 +0300

    Применены методы и свойства переменных

--- script.js
+++ script.js
@@ -2,12 +2,20 @@
 
 const title = "Учебный проект: JS 23.0";
 const screens = "Простые, Сложные, Интерактивные";
-const screenPrice = 1200;
-const rollback = 99;
+const screenPrice = 20000;
+const rollback = 15;
 const fullPrice = 150000;
 const adaptive = true;
 
-alert("Первый скрипт");
-console.log("Лог сообщения");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-// ветка lesson01
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+
+console.log(screens.toLowerCase().split(", "));
+
+console.log("Откат посреднику за работу " + Math.round((fullPrice * rollback) / 100) + " рублей");
